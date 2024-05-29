263361075e0b3088bbf495299774fc40ab6ec38c
Daniel <danielbolaji99@gmail.com>
2024-05-28 21:37:41 -0700

    Added few screens for the sign up

---
+++ app/src/main/java/com/example/play/screens/Contact.kt
@@ -0,0 +1,29 @@
+package com.example.play.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.play.R
+import com.example.play.databinding.FragmentContactBinding
+
+class Contact : Fragment() {
+    private lateinit var binding: FragmentContactBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentContactBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.button.setOnClickListener {
+            findNavController().navigate(R.id.action_contact_to_password2)
+        }
+    }
+}
